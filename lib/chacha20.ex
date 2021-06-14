0a6b76a0403227540536da32306567ef3c091ed5
Vahid Fazlollhzade Soraki <vfsoraki@gmail.com>
2021-06-14 07:35:33 +0430

    Now accepts bitstrings as input too Also adds some tests

--- lib/chacha20.ex
+++ lib/chacha20.ex
@@ -17,21 +17,38 @@ defmodule Chacha20 do
   defp run_chacha12(_data, _key, _nonce), do: error()
   defp run_chacha8(_data, _key, _nonce), do: error()
 
-  @spec init(algo(), list(integer()), list(integer())) :: t()
+  @spec init(algo(), list(integer()) | bitstring(), list(integer()) | bitstring()) :: t()
   def init(algo, key, nonce), do: %__MODULE__{algo: algo, key: key, nonce: nonce}
 
-  @spec encrypt(t(), list(integer())) :: {:ok, list(integer())} | {:error, atom()}
+  @spec encrypt(t(), list(integer()) | bitstring()) :: {:ok, bitstring()} | {:error, atom()}
   def encrypt(%__MODULE__{algo: :chacha20, key: key, nonce: nonce}, data) do
-    run_chacha20(data, key, nonce)
+    convert_and_run({data, key, nonce}, &run_chacha20/3)
   end
 
   def encrypt(%__MODULE__{algo: :chacha12, key: key, nonce: nonce}, data) do
-    run_chacha12(data, key, nonce)
+    convert_and_run({data, key, nonce}, &run_chacha12/3)
   end
 
   def encrypt(%__MODULE__{algo: :chacha8, key: key, nonce: nonce}, data) do
-    run_chacha8(data, key, nonce)
+    convert_and_run({data, key, nonce}, &run_chacha8/3)
   end
 
   def encrypt(_, _), do: {:error, :algo_not_valid}
+
+  defp convert_and_run({data, key, nonce}, fun) do
+    args =
+      [data, key, nonce]
+      |> Enum.map(&convert_to_charlist/1)
+
+    case apply(fun, args) do
+      {:ok, result} -> {:ok, convert_to_string(result)}
+      {:error, _} = err -> err
+    end
+  end
+
+  defp convert_to_charlist(t) when is_list(t), do: t
+  defp convert_to_charlist(t) when is_bitstring(t), do: :binary.bin_to_list(t)
+
+  defp convert_to_string(t) when is_list(t), do: to_string(t)
+  defp convert_to_string(t) when is_binary(t), do: t
 end
--- test/chacha20_test.exs
+++ test/chacha20_test.exs
@@ -2,7 +2,27 @@ defmodule Chacha20Test do
   use ExUnit.Case
   doctest Chacha20
 
-  test "greets the world" do
-    assert Chacha20.hello() == :world
+  @key <<1::8*32>>
+  @nonce <<1::8*12>>
+
+  test "chacha20 fixed input" do
+    c = Chacha20.init :chacha20, @key, @nonce
+    assert Chacha20.encrypt(c, [1,2,3]) == {:ok, <<194, 135, 81, 69>>}
+    assert Chacha20.encrypt(c, <<1,2,3>>) == {:ok, <<194, 135, 81, 69>>}
+    assert Chacha20.encrypt(c, "123") == {:ok, "·au"}
+  end
+
+  test "tests chacha12" do
+    c = Chacha20.init :chacha12, @key, @nonce
+    assert Chacha20.encrypt(c, [1,2,3]) == {:ok, <<11, 49, 20>>}
+    assert Chacha20.encrypt(c, <<1,2,3>>) == {:ok, <<11, 49, 20>>}
+    assert Chacha20.encrypt(c, "123") == {:ok, <<59, 1, 36>>}
+  end
+
+  test "tests chacha8" do
+    c = Chacha20.init :chacha8, @key, @nonce
+    assert Chacha20.encrypt(c, [1,2,3]) == {:ok, <<194, 132, 24, 94>>}
+    assert Chacha20.encrypt(c, <<1,2,3>>) == {:ok, <<194, 132, 24, 94>>}
+    assert Chacha20.encrypt(c, "123") == {:ok, "´(n"}
   end
 end
